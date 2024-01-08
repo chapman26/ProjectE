8d950515a5ec60af49db88c6655c6fb2db3abd5d
samchapman <samchapman@192.168.1.245>
2024-01-08 00:04:06 -0500

    I love Christmas

---
+++ src/e/DemoE.java
@@ -0,0 +1,14 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello again E");
+		
+		System.out.println("I miss Christmas");
+		
+		System.out.println("But next year will be awesome");
+	}
+
+}
